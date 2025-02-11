3f9aec39d83c2c0f41eddb55bfa5dd28771f5410
Nicolas Garric <nicolasdfgarric@gmail.com>
2025-02-11 11:42:48 +0100

    Bullet done && damage enemy done && focus UX

--- godot-survivor/mob.gd
+++ godot-survivor/mob.gd
@@ -6,6 +6,7 @@ var basic_mob_health = 3
 
 func _ready() -> void:
 	player = get_node("/root/Game/player")
+	%Slime.play_walk()
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
@@ -13,7 +14,13 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func take_damage():
+	
 	basic_mob_health -= 1
+	%Slime.play_hurt()
 
-	if health == 0:
+	if basic_mob_health == 0:
 		queue_free()
+		const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
+		var smoke = SMOKE_SCENE.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
