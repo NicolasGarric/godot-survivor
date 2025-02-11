bb93bc8d30a931e156bd61bda76fc021a4e9d621
Nicolas Garric <nicolasdfgarric@gmail.com>
2025-02-11 11:22:46 +0100

    Adding gun script && ennemy damage && bullets targeting

--- godot-survivor/mob.gd
+++ godot-survivor/mob.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var player
 var basic_mob_speed = 300.0
+var basic_mob_health = 3
 
 func _ready() -> void:
 	player = get_node("/root/Game/player")
@@ -10,3 +11,9 @@ func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * basic_mob_speed
 	move_and_slide()
+
+func take_damage():
+	basic_mob_health -= 1
+
+	if health == 0:
+		queue_free()
